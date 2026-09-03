45b2a0777fc9100e3c831b4f9c818e1eca073e46
danalistiq-a11y <danalistiq@gmail.com>
2026-09-03 21:46:02 +0200

    Create ClientPacketListenerMixin.java

---
+++ src/main/java/com/danalistiq/potionlooker/ClientPacketListenerMixin.java
@@ -0,0 +1,32 @@
+package com.danalistiq.potionlooker.mixin;
+
+import net.minecraft.client.multiplayer.ClientPacketListener;
+import net.minecraft.network.protocol.game.ClientboundUpdateMobEffectPacket;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ClientPacketListener.class)
+public class ClientPacketListenerMixin {
+
+    @Inject(
+            method = "handleUpdateMobEffect",
+            at = @At("HEAD")
+    )
+    private void potionLooker$effectPacket(
+            ClientboundUpdateMobEffectPacket packet,
+            CallbackInfo ci
+    ) {
+        System.out.println(
+                "POTIONLOOKER EFFECT PACKET: entity="
+                        + packet.getEntityId()
+                        + " effect="
+                        + packet.getEffect()
+                        + " amplifier="
+                        + packet.getEffectAmplifier()
+                        + " duration="
+                        + packet.getEffectDurationTicks()
+        );
+    }
+}
